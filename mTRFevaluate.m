c58d1803e1a2a764d8a4c8f8f7e935dcdf3a6c7e
Mick Crosse <mickcrosse@gmail.com>
2020-02-28 00:39:22 -0800

    Create mTRFevaluate.m

---
+++ mTRFevaluate.m
@@ -0,0 +1,154 @@
+function [acc,err] = mTRFevaluate(y,yhat,varargin)
+%MTRFEVALUATE  Evaluate model performance.
+%   ACC = MTRFEVALUATE(Y,YHAT) returns the accuracy of the prediction YHAT
+%   relative to the ground truth Y based on the Pearson correlation
+%   coefficient.
+%
+%   [ACC,ERR] = MTRFEVALUATE(Y,YHAT) returns the error of the prediction
+%   YHAT relative to the ground truth Y based on the mean squared error
+%   (MSE).
+%
+%   [...] = MTRFEVALUATE(...,'PARAM1',VAL1,'PARAM2',VAL2,...) specifies
+%   additional parameters and their values. Valid parameters are the
+%   following:
+%
+%       Parameter   Value
+%       'dim'       A scalar specifying the dimension to work along: pass
+%                   in 1 to work along the columns (default), or 2 to work
+%                   along the rows. Applies to both STIM and RESP.
+%       'acc'       A string specifying the accuracy metric to use:
+%                       'Pearson'   Pearson's linear correlation
+%                                   coefficient (default): suitable for
+%                                   data with a linear relationship
+%                       'Spearman'  Spearman's rank correlation
+%                                   coefficient: suitable for data with
+%                                   non-linear relationship
+%       'err'       A string specifying the error metric to use:
+%                       'msc'       Mean square error (default): take the
+%                                   square root to convert it to the
+%                                   original units of the data (i.e., RMSE)
+%                       'mae'       Mean absolute error: more robust to
+%                                   outliers than MSE
+%
+%   See mTRFdemos for examples of use.
+%
+%   See also CORR, CORRCOEF, TIEDRANK, MSE, MAE.
+%
+%   mTRF-Toolbox https://github.com/mickcrosse/mTRF-Toolbox
+
+%   References:
+%      [1] Crosse MC, Di Liberto GM, Bednar A, Lalor EC (2016) The
+%          multivariate temporal response function (mTRF) toolbox: a MATLAB
+%          toolbox for relating neural signals to continuous stimuli. Front
+%          Hum Neurosci 10:604.
+
+%   Authors: Mick Crosse <mickcrosse@gmail.com>
+%   Copyright 2014-2020 Lalor Lab, Trinity College Dublin.
+
+% Parse input arguments
+arg = parsevarargin(varargin);
+
+% Set default values
+if nargin < 3 || isempty(arg.acc)
+    arg.acc = 'Pearson';
+end
+if nargin < 4 || isempty(arg.err)
+    arg.err = 'mse';
+end
+
+% Get dimensions
+if arg.dim == 2
+    y = y';
+    yhat = yhat';
+end
+n = size(y,1);
+if size(yhat,1) ~= n
+    error(['Y and YHAT arguments must have the same number of '...
+        'observations.'])
+end
+
+% Compute accuracy
+switch arg.acc
+    case 'Spearman'
+        y = num2rank(y);
+        yhat = num2rank(yhat);
+end
+muxy = sum(y).*sum(yhat)/n;
+sdxy = sqrt((sum(y.^2)-(sum(y).^2)/n).*(sum(yhat.^2)-(sum(yhat).^2)/n));
+acc = (sum(y.*yhat)-muxy)./sdxy;
+
+% Compute error
+switch arg.err
+    case 'mse'
+        exponent = 2;
+    case 'mae'
+        exponent = 1;
+end
+err = sum(abs(y-yhat).^exponent,1)/n;
+
+function xranked = num2rank(x)
+%NUM2RANK  Rank numbers and average ties.
+%   XRANKED = NUM2RANK(X) ranks the values in each column of X and averages
+%   any tied ranks.
+
+% Initialize variables
+[nobs,nvar] = size(x);
+xranked = zeros(nobs,nvar);
+
+for i = 1:nvar
+    
+    % Sort data in ascending order
+    [xsort,order] = sort(x(:,i));
+    ranks = (1:nobs)';
+    
+    % Find ties
+    ties = xsort(1:nobs-1) >= xsort(2:nobs);
+    idx = [find(ties);nobs+2];
+    maxt = numel(idx);
+    
+    % Average ties
+    ctr = 1;
+    while ctr < maxt
+        m = idx(ctr); n = 2;
+        while idx(ctr+1) == idx(ctr)+1
+            ctr = ctr+1; n = n+1;
+        end
+        ranks(m:m+n-1) = sum(ranks(m:m+n-1))/n;
+        ctr = ctr+1;
+    end
+    
+    % Order ranks
+    xranked(order) = ranks;
+    
+end
+
+function arg = parsevarargin(varargin)
+%PARSEVARARGIN  Parse input arguments.
+%   [PARAM1,PARAM2,...] = PARSEVARARGIN('PARAM1',VAL1,'PARAM2',VAL2,...)
+%   parses the input arguments of the main function.
+
+% Create parser object
+p = inputParser;
+
+% Dimension to work along
+errorMsg = 'It must be a positive integer scalar within indexing range.';
+validFcn = @(x) assert(x==1||x==2,errorMsg);
+addParameter(p,'dim',1,validFcn);
+
+% Accuracy metric
+accOptions = {'Pearson','Spearman'};
+validFcn = @(x) any(validatestring(x,accOptions));
+addParameter(p,'acc','Pearson',validFcn);
+
+% Error metric
+errOptions = {'mse','mae'};
+validFcn = @(x) any(validatestring(x,errOptions));
+addParameter(p,'err','msc',validFcn);
+
+% Parse input arguments
+parse(p,varargin{1,1}{:});
+arg = p.Results;
+
+% Redefine partially-matched strings
+arg.acc = validatestring(arg.acc,accOptions);
+arg.err = validatestring(arg.err,errOptions);
