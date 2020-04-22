5f8e46c076802381c8b372cb33eb9bef0af4dc20
Mick Crosse <mickcrosse@gmail.com>
2020-04-22 19:37:44 -0400

    Update mTRFattnevaluate.m

--- mtrf/mTRFattnevaluate.m
+++ mtrf/mTRFattnevaluate.m
@@ -1,19 +1,20 @@
-function [adi,d] = mTRFattnevaluate(acc1,acc2,varargin)
-%MTRFATTNEVALUATE  Evaluate the accuracy of an attention decoder.
-%   ADI = MTRFATTNEVALUATE(ACC1,ACC2) returns the attention decoding
-%   index (ADI) based on the proportion of folds where the accuracy for
-%   attended stimuli ACC1 was greater than that of the unattended stimuli
-%   ACC2.
+function [acc,d] = mTRFattnevaluate(r1,r2,varargin)
+%MTRFATTNEVALUATE  Evaluate the performance of an attention decoder.
+%   ACC = MTRFATTNEVALUATE(R1,R2) returns the accuracy of an attention
+%   decoder based on the proportion of observations where the correlation
+%   for the attended stimulus R1 was greater than that of the unattended
+%   stimulus R2 as per O'Sullivan et al. (2015). Note, metrics other than
+%   correlation can be input to this function to evaluate performance.
 %
-%   If ACC1 or ACC2 are matrices, it is assumed that the rows correspond to
+%   If R1 or R2 are matrices, it is assumed that the rows correspond to
 %   observations and the columns to variables, unless otherwise stated via
 %   the 'dim' parameter (see below). If they are vectors, it is assumed
 %   that the first non-singleton dimension corresponds to observations.
-%   ACC1 and ACC2 must have the same number of observations.
-
-%   [ADI,D] = MTRFATTNEVALUATE(ACC1,ACC2) returns the sensitivity index
-%   based on d', where the accuracy for attended stimuli is considered
-%   signal and that of the unattended stimuli is considered noise.
+%   R1 and R2 must have the same number of observations.
+%
+%   [ACC,D] = MTRFATTNEVALUATE(R1,R2) returns the sensitivity index based
+%   on d', where R1 is considered signal and R2 is considered noise as per
+%   de Cheveigné et al. (2018).
 %
 %   [...] = MTRFATTNEVALUATE(...,'PARAM1',VAL1,'PARAM2',VAL2,...) specifies
 %   additional parameters and their values. Valid parameters are the
@@ -22,17 +23,20 @@ function [adi,d] = mTRFattnevaluate(acc1,acc2,varargin)
 %       Parameter   Value
 %       'dim'       A scalar specifying the dimension to work along: pass
 %                   in 1 to work along the columns (default), or 2 to work
-%                   along the rows. Applies to both ACC1 and ACC2.
+%                   along the rows. Applies to both R1 and R2.
 %
 %   See also DPRIME.
 %
 %   mTRF-Toolbox https://github.com/mickcrosse/mTRF-Toolbox
 
 %   References:
-%      [1] Crosse MC, Di Liberto GM, Bednar A, Lalor EC (2016) The
-%          multivariate temporal response function (mTRF) toolbox: a MATLAB
-%          toolbox for relating neural signals to continuous stimuli. Front
-%          Hum Neurosci 10:604.
+%      [1] O'Sullivan JA, Power AJ, Mesgarani N, Rajaram S, Foxe JJ, Shinn-
+%          Cunningham BG, Slaney M, Shamma SA, Lalor EC (2015) Attentional
+%          Selection in a Cocktail Party Environment Can Be Decoded from
+%          Single-Trial EEG. Cereb Cortex 25(7):1697-1706.
+%      [2] de Cheveigné A, Wong DE, Di Liberto GM, Hjortkjær J, Slaney M,
+%          Lalor EC (2015) Decoding the auditory brain with canonical
+%          component analysis. NeuroImage 172:206-216.
 
 %   Authors: Mick Crosse <mickcrosse@gmail.com>
 %   Copyright 2014-2020 Lalor Lab, Trinity College Dublin.
@@ -42,22 +46,27 @@ arg = parsevarargin(varargin);
 
 % Get dimensions
 if arg.dim == 2
-    acc1 = acc1';
-    acc2 = acc2';
+    r1 = r1';
+    r2 = r2';
 end
-n = size(acc1,1);
-if size(acc2,1) ~= n
-    error(['ACC1 and ACC2 arguments must have the same number of '...
-        'observations.'])
+n = size(r1,1);
+if size(r2,1) ~= n
+    error('R1 and R2 arguments must have the same number of observations.')
 end
 
-% Compute means
-m1 = sum(acc1,1)/n;
-m2 = sum(acc2,1)/n;
+% Compute accuracy
+acc = sum(r1>r2,1)/n;
 
-% Compute ADI and d'
-adi = sum(acc1>acc2,1)/n;
-d = (m1 - m2)./sqrt((sum((acc1-m1).^2) + sum((acc2-m2).^2))/(n-1)/2);
+if nargout > 1
+    
+    % Get mean values
+    m1 = sum(r1,1)/n;
+    m2 = sum(r2,1)/n;
+    
+    % Compute d'
+    d = (m1 - m2)./sqrt((sum((r1 - m1).^2) + sum((r2 - m2).^2))/(n - 1)/2);
+    
+end
 
 function arg = parsevarargin(varargin)
 %PARSEVARARGIN  Parse input arguments.
